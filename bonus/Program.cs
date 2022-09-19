211560bbaf006a2d069bbc6890bf3a39d5776354
mikle <mr.mikle@mail.ru>
2022-09-19 15:48:56 +0300

    выполнили пример бонус с возрастом

---
+++ bonus/Program.cs
@@ -0,0 +1,8 @@
+﻿// в какой греппе возраста вы находитесь
+Console.WriteLine($"Введите Ваш возраст: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<12) {Console.Write($"ребенок");}
+else if (a<21) {Console.Write($"подросток");}
+else if (a<65) {Console.Write($"взрослый");}
+else if (a<85) {Console.Write($"пожилой");}
+else {Console.Write($"долгожитель");}
