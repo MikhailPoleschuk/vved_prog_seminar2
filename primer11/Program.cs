92dc1bc6fb0fa41fc56b386cef9fc8feb6d4a5bd
mikle <mr.mikle@mail.ru>
2022-09-19 15:06:10 +0300

    выполнили пример 11, уложился лишь в 17 мин, лучше сначал делать блок схему, проверять где >=, а также проверять математику

--- primer11/Program.cs
+++ primer11/Program.cs
@@ -1,3 +1,21 @@
-﻿// 11
-int x= new Random().Next(0,1000);
+﻿// 11 вывод случайного целого трехзначного числа, удаляем второе и пишем итоговое двузначное
+int x= new Random().Next(100,1000);
 Console.WriteLine(x);
+int N=x;
+while (N > 100) N=N-100;
+int dv = N;
+while (N>=10) N=N-10;
+int Edinicy = N;
+int desytky = (dv-N)/10;
+int Sotny = (x - dv)/100;
+int itog = Sotny*10 + Edinicy;
+Console.WriteLine($"Единицы {Edinicy}");
+Console.WriteLine($"Десятки {desytky}");
+Console.WriteLine($"Сотни {Sotny}");
+Console.WriteLine($"Итог {itog}");
+
+
+
+
+
+
