28681fa5efccd6136400d6fd5ddef68bca3e1a9e
mikle <mr.mikle@mail.ru>
2022-09-19 15:31:35 +0300

    выполнили пример 14

---
+++ primer14/Program.cs
@@ -0,0 +1,7 @@
+﻿// 14
+Console.WriteLine($"Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ost1= a%7; // находим остаток от деления на 7
+int ost2= a%23; // находим остаток от деления на 23
+if (ost1>0 & ost1>0) {Console.Write($" нет");}
+else {Console.Write($" да");}
