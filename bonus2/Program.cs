aa5441b6e12f2a43ce601ecf05f276ac82807869
mikle <mr.mikle@mail.ru>
2022-09-19 15:58:53 +0300

    выполнили пример бонус с суммой положительных из 3х чисел

---
+++ bonus2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Введите 3 числа и посчитайте сумму только положительных чисел
+int i = 0;
+int Sum = 0;
+while (i < 3)
+{
+    Console.WriteLine($"Введите число: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a > 0) { Sum = Sum + a; }
+    i++;
+}
+Console.WriteLine(Sum);
