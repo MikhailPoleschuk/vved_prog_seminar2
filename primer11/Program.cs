6a0210b057fb0264d005ff317066e80298b2e860
mikle <mr.mikle@mail.ru>
2022-09-19 14:45:21 +0300

    Начали и выполнили пример 9

---
+++ primer11/Program.cs
@@ -0,0 +1,3 @@
+﻿// 11
+int x= new Random().Next(0,1000);
+Console.WriteLine(x);
---
+++ primer9/Program.cs
@@ -0,0 +1,19 @@
+﻿// случайное число отрезка 10,99 и показывает наибольшую цифру числа
+int x= new Random().Next(10,100);
+Console.WriteLine($"Нам выпало число {x}");
+int N = x;
+while (N>10) N=N-10;
+int Edinica=N;
+int Desytky=(x-N)/10;
+if (Desytky==Edinica) 
+{
+    Console.WriteLine($"Цифры равны {Desytky} ");
+}
+else if (Desytky>Edinica)
+{
+Console.WriteLine($"Цифра {Desytky} больше");
+}
+else 
+{
+    Console.WriteLine($"Цифра {Edinica} больше");
+}
