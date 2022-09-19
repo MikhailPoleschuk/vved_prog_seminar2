43bedf7dbcde1fe7921d500f0122b0fcc705ff46
mikle <mr.mikle@mail.ru>
2022-09-19 15:35:42 +0300

    выполнили пример 16

---
+++ primer16/Program.cs
@@ -0,0 +1,7 @@
+﻿// 16
+Console.WriteLine($"Введите Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b || b == a*a) {Console.Write($" да");}
+else {Console.Write($" нет");}
