b1796af0fe604e75f4b22fb44f4451a2ee93fbfb
mikle <mr.mikle@mail.ru>
2022-09-19 15:23:44 +0300

    выполнили пример 12

---
+++ primer12/Program.cs
@@ -0,0 +1,24 @@
+﻿// 12
+int ost;
+Console.WriteLine($"Введите Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>=b)
+{
+    ost= a%b;
+}
+else
+{
+    ost = b%a;
+}
+Console.Write($" Первое число {a}");
+Console.Write($" Второе число {b}");
+if (ost > 0) 
+{
+        Console.WriteLine($" не кратно остаток {ost}");
+}
+else 
+{
+    Console.WriteLine($" кратно");
+}
